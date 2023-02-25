db92b01469499457a0f054c714be9dfc52edc16a
Elton Santos <eltonaxl@hotmail.com>
2023-02-25 16:40:48 -0300

    Add icon website

--- meuslinks/src/components/Menu/index.jsx
+++ meuslinks/src/components/Menu/index.jsx
@@ -1,10 +1,17 @@
-import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { BsGithub, BsGlobe2, BsLinkedin } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import "./styles.css";
 
 export function Menu() {
   return (
     <div className="menu">
+      <a
+        className="social"
+        target="_blank"
+        href="https://eltonmelosantos.com.br"
+      >
+        <BsGlobe2 color="#fff" size={25} />
+      </a>
       <a
         className="social"
         target="_blank"
