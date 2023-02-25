8a4fdc4506b7da64069826bcdd6a7111079ebc36
Elton Santos <eltonaxl@hotmail.com>
2023-02-25 07:03:16 -0300

    Initial commit

--- meuslinks/src/App.jsx
+++ meuslinks/src/App.jsx
@@ -1,7 +1,12 @@
 import "./app.css";
+import { Home } from "./pages/Home";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ meuslinks/src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+export function Home() {
+  return (
+    <div className="container-home">
+      <div>
+        <img src="/logo.png" alt="Sujeito Link Logo" />
+      </div>
+    </div>
+  );
+}
